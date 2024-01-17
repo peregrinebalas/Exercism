307c12fc1e166ca0e5b39eb789748a7e8293c95d
Peregrine Balas <peregrinereedbalas@gmail.com>
2024-01-16 20:42:18 -0600

    submit

--- go/elons-toys/elons_toys.go
+++ go/elons-toys/elons_toys.go
@@ -3,24 +3,30 @@ package elon
 import "fmt"
 
 // TODO: define the 'Drive()' method
-func (c Car) Drive() {
-	c.battery -= c.batteryDrain
-	c.distance += c.speed
+func (c *Car) Drive() {
+	if c.battery >= c.batteryDrain {
+		c.battery -= c.batteryDrain
+		c.distance += c.speed
+	}
 }
 
 // TODO: define the 'DisplayDistance() string' method
-func (c Car) DisplayDistance() string {
+func (c *Car) DisplayDistance() string {
 	return fmt.Sprintf("Driven %d meters", c.distance)
 }
 
 // TODO: define the 'DisplayBattery() string' method
-func (c Car) DisplayBattery() string {
-	return fmt.Sprintf("Battery at %d\%", c.battery)
+func (c *Car) DisplayBattery() string {
+	return fmt.Sprintf("Battery at %d%%", c.battery)
 }
 
 // TODO: define the 'CanFinish(trackDistance int) bool' method
-func (c Car) CanFinish(trackDistance int) bool {
-	
+func (c *Car) CanFinish(trackDistance int) bool {
+	batteryUnits := (c.battery / c.batteryDrain) // segments of battery
+	if batteryUnits*c.speed >= trackDistance {
+		return true
+	}
+	return false
 }
 
 // Your first steps could be to read through the tasks, and create
