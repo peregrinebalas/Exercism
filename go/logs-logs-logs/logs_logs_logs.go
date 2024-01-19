4ad98736cd31d3a179fbbc44b0ea1a2abdeffdb9
Peregrine Balas <peregrinereedbalas@gmail.com>
2024-01-18 18:22:34 -0600

    submit

--- go/logs-logs-logs/logs_logs_logs.go
+++ go/logs-logs-logs/logs_logs_logs.go
@@ -1,18 +1,40 @@
 package logs
 
+import "unicode/utf8"
+
+var appLogs = map[rune]string{
+	'❗': "recommendation",
+	'🔍': "search",
+	'☀': "weather",
+}
+
 // Application identifies the application emitting the given log.
 func Application(log string) string {
-	panic("Please implement the Application() function")
+	for _, char := range log {
+		value, exists := appLogs[char]
+		if exists {
+			return value
+		}
+	}
+	return "default"
 }
 
 // Replace replaces all occurrences of old with new, returning the modified log
 // to the caller.
 func Replace(log string, oldRune, newRune rune) string {
-	panic("Please implement the Replace() function")
+	newLog := ""
+	for _, char := range log {
+		if char == oldRune {
+			newLog += string(newRune)
+		} else {
+			newLog += string(char)
+		}
+	}
+	return newLog
 }
 
 // WithinLimit determines whether or not the number of characters in log is
 // within the limit.
 func WithinLimit(log string, limit int) bool {
-	panic("Please implement the WithinLimit() function")
+	return limit >= utf8.RuneCountInString(log)
 }
