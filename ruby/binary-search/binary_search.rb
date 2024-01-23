c46f960242b6bd80c12968e7310bf4c05fa2e4e2
Peregrine Balas <peregrinereedbalas@gmail.com>
2024-01-22 22:25:55 -0600

    passing

--- ruby/binary-search/binary_search.rb
+++ ruby/binary-search/binary_search.rb
@@ -1,16 +1,38 @@
-=begin
-Write your code for the 'Binary Search' exercise in this file. Make the tests in
-`binary_search_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/binary-search` directory.
-=end
 class BinarySearch
-    def initialize(list) do
+    attr_reader :list
+
+    def initialize(list)
         @list = list
     end
 
-    def search do
+    def search_for(n)
+        len = list.length
+        first = 0
+        last = len - 1
+        i = len/2
         
+        if list[i] == n then
+            return i
+        elsif list.empty? then
+            return nil
+        end
+
+        while first != last do
+            if list[i] == n then
+                return i
+            elsif first + 1 == last then
+                break
+            else
+                if list[i] > n then
+                    last = i
+                    node = ((last - first) / 2)
+                    i = i - (node > 0 ? node : 1)
+                else
+                    first = i
+                    node = ((last - first) / 2) 
+                    i = i + (node > 0 ? node : 1)
+                end
+            end
+        end
     end
 end
