f521ee81640d02bf96cf53e4280bf8a74ce22358
Peregrine Balas <peregrinereedbalas@gmail.com>
2024-01-24 20:59:00 -0600

    rm comment

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -1,10 +1,3 @@
-=begin
-Write your code for the 'Matrix' exercise in this file. Make the tests in
-`matrix_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/matrix` directory.
-=end
 class Matrix
     attr_reader :matrix
     def initialize(matrix_str)
