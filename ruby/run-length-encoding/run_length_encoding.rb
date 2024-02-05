085238ead81d43fdf86c4c077f15f2b46a320cc5
Peregrine Balas <peregrinereedbalas@gmail.com>
2024-02-05 15:22:09 -0600

    gsub

--- ruby/run-length-encoding/run_length_encoding.rb
+++ ruby/run-length-encoding/run_length_encoding.rb
@@ -1,22 +1,9 @@
 class RunLengthEncoding
     def self.encode(input)
-        input.split('')
-             .slice_when { |before, after| before != after }
-             .reduce('') { |output, chunk| output << encode_chunk(chunk) }
+        input.gsub(/(.)\1+/) { |x| "#{x.size}#{x[0]}"}
     end
 
     def self.decode(input)
-        input.scan(/\d+.|./)
-             .reduce('') { |output, chunk| output << decode_chunk(chunk) }
-    end
-
-    private
-
-    def self.encode_chunk(chunk)
-        chunk.size==1 ? chunk[0] : "#{chunk.size}#{chunk[0]}"
-    end
-
-    def self.decode_chunk(chunk)
-        chunk.size==1 ? chunk[0] : chunk[-1]*chunk[0, chunk.size-1].to_i
+        input.gsub(/\d+./) { |x| x[-1] * x.to_i }
     end
 end
