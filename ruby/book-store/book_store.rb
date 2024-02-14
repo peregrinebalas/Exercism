3042adb55f6a5b946ae9857d4cc509fefb87af50
Peregrine Balas <peregrinereedbalas@gmail.com>
2024-02-13 23:53:05 -0600

    refactor

--- ruby/book-store/book_store.rb
+++ ruby/book-store/book_store.rb
@@ -3,38 +3,33 @@ module BookStore
         counts = basket.reduce(Hash.new(0)) { |counts, book| counts[book] += 1; counts }
         basket = basket.sort_by { |book| counts[book] }
         batches = basket.reverse.reduce([[]]) do |batches, book|
-            i = 0
-            while batches[i] && batches[i].include?(book) do
-                i += 1
-            end
-
-            batch = batches.find do |batch|
-                (batch.length == 3 &&
-                !batch.include?(book))
-            end
-
+            batch = batches.find { |batch| batch.length == 3 && !batch.include?(book) }
             if batch then
                 batch << book
-            elsif batches[i]
-                batches[i] << book
             else
-                batches << [book]
+                i = 0; i += 1 while batches[i] && batches[i].include?(book)
+                batches[i] ? batches[i] << book : batches << [book]
             end
 
             batches
         end
 
-        batches.reduce(0) do |price, batch|
-            discount =
-            case batch.size
-            when 4,5
-                 (batch.size * 0.05)
-            when 2,3
-                ((batch.size - 1) * 0.05)
-            else
-                0
-            end
-            price += (batch.size * 8) - (discount * (batch.size * 8))
+        batches.reduce(0) do |total, batch|
+            price = batch.size * 8
+            total += price - discount(batch.size) * price
+        end
+    end
+
+    private
+    
+    def self.discount(group)
+        case group
+        when 4,5
+             (group * 0.05)
+        when 2,3
+            ((group - 1) * 0.05)
+        else
+            0
         end
     end
 end
