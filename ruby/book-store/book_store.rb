8dd6c86547947efc0680a147c9ade6f2ddd1d582
Peregrine Balas <peregrinereedbalas@gmail.com>
2024-02-13 22:19:59 -0600

    wip

--- ruby/book-store/book_store.rb
+++ ruby/book-store/book_store.rb
@@ -1,7 +1,54 @@
-=begin
-Write your code for the 'Book Store' exercise in this file. Make the tests in
-`book_store_test.rb` pass.
+module BookStore
+    def self.calculate_price(basket)
+        pp basket
+        counts = basket.reduce({}) do |counts, book|
+            counts[book] ? counts[book] += 1 : counts[book] = 1
+            counts
+        end
+        counts = counts.to_a.sort { |a,b| b[1] <=> a[1] }
+        pp counts.to_h
+        batches = counts.to_h.reduce([]) do |batches, (book, count)|
+            
+            count.times do
+                batches = if count > batches.size then
+                    Array.new(count) { [book] }
+                else
+                    countdown = count
+                    batches.map do |batch|
+                        if countdown > 0 && !batch.include?(book) then
+                            countdown -= 1
+                            batch << book
+                        else
+                            batch
+                        end
+                    end
+                end
+            end
+            batches
+        end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/book-store` directory.
-=end
+        pp batches
+        # batches = basket.reduce([[]]) do |batches, book|
+        #     i = 0
+        #     while batches[i] && batches[i].include?(book) do
+        #         i += 1
+        #     end
+        #     min = batches.min { |a, b| a.length <=> b.length }
+        #     batches[i] ? batches[i] << book : batches << [book]
+        #     batches
+        # end
+
+        batches.reduce(0) do |price, batch|
+            discount =
+            case batch.size
+            when 4,5
+                 (batch.size * 0.05)
+            when 2,3
+                ((batch.size - 1) * 0.05)
+            else
+                0
+            end
+            price += (batch.size * 8) - (discount * (batch.size * 8))
+        end
+    end
+end
--- ruby/book-store/book_store_test.rb
+++ ruby/book-store/book_store_test.rb
@@ -3,43 +3,36 @@ require_relative 'book_store'
 
 class BookStoreTest < Minitest::Test
   def test_only_a_single_book
-    # skip
     basket = [1]
     assert_in_delta 8.00, BookStore.calculate_price(basket), 0.001
   end
 
   def test_two_of_the_same_book
-    skip
     basket = [2, 2]
     assert_in_delta 16.00, BookStore.calculate_price(basket), 0.001
   end
 
   def test_empty_basket
-    skip
     basket = []
     assert_in_delta 0.00, BookStore.calculate_price(basket), 0.001
   end
 
   def test_two_different_books
-    skip
     basket = [1, 2]
     assert_in_delta 15.20, BookStore.calculate_price(basket), 0.001
   end
 
   def test_three_different_books
-    skip
     basket = [1, 2, 3]
     assert_in_delta 21.60, BookStore.calculate_price(basket), 0.001
   end
 
   def test_four_different_books
-    skip
     basket = [1, 2, 3, 4]
     assert_in_delta 25.60, BookStore.calculate_price(basket), 0.001
   end
 
   def test_five_different_books
-    skip
     basket = [1, 2, 3, 4, 5]
     assert_in_delta 30.00, BookStore.calculate_price(basket), 0.001
   end
@@ -57,31 +50,26 @@ class BookStoreTest < Minitest::Test
   end
 
   def test_group_of_four_plus_group_of_two_is_cheaper_than_two_groups_of_three
-    skip
     basket = [1, 1, 2, 2, 3, 4]
     assert_in_delta 40.80, BookStore.calculate_price(basket), 0.001
   end
 
   def test_two_each_of_first_four_books_and_one_copy_each_of_rest
-    skip
     basket = [1, 1, 2, 2, 3, 3, 4, 4, 5]
     assert_in_delta 55.60, BookStore.calculate_price(basket), 0.001
   end
 
   def test_two_copies_of_each_book
-    skip
     basket = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
     assert_in_delta 60.00, BookStore.calculate_price(basket), 0.001
   end
 
   def test_three_copies_of_first_book_and_two_each_of_remaining
-    skip
     basket = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 1]
     assert_in_delta 68.00, BookStore.calculate_price(basket), 0.001
   end
 
   def test_three_each_of_first_two_books_and_two_each_of_remaining_books
-    skip
     basket = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 1, 2]
     assert_in_delta 75.20, BookStore.calculate_price(basket), 0.001
   end
@@ -99,13 +87,11 @@ class BookStoreTest < Minitest::Test
   end
 
   def test_one_group_of_one_and_four_is_cheaper_than_one_group_of_two_and_three
-    skip
     basket = [1, 1, 2, 3, 4]
     assert_in_delta 33.60, BookStore.calculate_price(basket), 0.001
   end
 
   def test_one_group_of_one_and_two_plus_three_groups_of_four_is_cheaper_than_one_group_of_each_size
-    skip
     basket = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5]
     assert_in_delta 100.00, BookStore.calculate_price(basket), 0.001
   end
