9dbc780d7e92acd5a01faeef58aea9b78a940f46
Peregrine Balas <peregrinereedbalas@gmail.com>
2024-02-25 22:43:31 -0600

    solve

--- ruby/linked-list/linked_list.rb
+++ ruby/linked-list/linked_list.rb
@@ -1,2 +1,21 @@
 class Deque
+    def initialize
+        @list = []
+    end
+    
+    def push(x)
+        @list.push(x)
+    end
+    
+    def pop
+        @list.pop
+    end
+    
+    def unshift(x)
+        @list.unshift(x)
+    end
+    
+    def shift
+        @list.shift
+    end
 end
--- ruby/linked-list/linked_list_test.rb
+++ ruby/linked-list/linked_list_test.rb
@@ -11,7 +11,6 @@ class DequeTest < Minitest::Test
   end
 
   def test_push_shift
-    skip
     deque = Deque.new
     deque.push(10)
     deque.push(20)
@@ -20,7 +19,6 @@ class DequeTest < Minitest::Test
   end
 
   def test_unshift_shift
-    skip
     deque = Deque.new
     deque.unshift(10)
     deque.unshift(20)
@@ -29,7 +27,6 @@ class DequeTest < Minitest::Test
   end
 
   def test_unshift_pop
-    skip
     deque = Deque.new
     deque.unshift(10)
     deque.unshift(20)
@@ -38,7 +35,6 @@ class DequeTest < Minitest::Test
   end
 
   def test_example
-    skip
     deque = Deque.new
     deque.push(10)
     deque.push(20)
@@ -53,7 +49,6 @@ class DequeTest < Minitest::Test
   end
 
   def test_pop_to_empty
-    skip
     deque = Deque.new
     deque.push(10)
     assert_equal 10, deque.pop
@@ -62,7 +57,6 @@ class DequeTest < Minitest::Test
   end
 
   def test_shift_to_empty
-    skip
     deque = Deque.new
     deque.unshift(10)
     assert_equal 10, deque.shift
