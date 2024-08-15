8e7ca8709595786be2bfd3b0d0c4dd96356ab091
Peregrine Balas <peregrinereedbalas@gmail.com>
2024-08-15 18:19:32 -0500

    minor changes

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,10 +1,3 @@
-=begin
-Write your code for the 'Grains' exercise in this file. Make the tests in
-`grains_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/grains` directory.
-=end
 class Grains
     def self.square(n)
         raise ArgumentError if n < 1 || n > 64
@@ -23,7 +16,7 @@ class Grains
 
     def self.set_values
         i = 1
-        v = (1...64).to_a
+        v = *(1...64)
         v.last.times do
             v[i] = v[i-1] * 2
             i += 1
